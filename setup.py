38e5b45fcc7119a60f0f3a73fc6d365f10a5fe7d
Monte Flora <monte.flora@weather.com>
2025-10-15 18:25:10 +0000

    removing envs and extra files; update setup.py

--- setup.py
+++ setup.py
@@ -1,49 +1,49 @@
-# Always prefer setuptools over distutils
+# setup.py - UPDATED VERSION
 
-import setuptools  # this is the "magic" import
-
-import ml_workflow
-
-from numpy.distutils.core import setup, Extension
-
-#from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 import pathlib
+import re
 
 here = pathlib.Path(__file__).parent.resolve()
 
 # Get the long description from the README file
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
+def get_version():
+    """Get version, with fallback"""
+    return '1.1.0'  # Simple and works!
+
 setup(
     name='ml_workflow', 
-    version=ml_workflow.__version__,
+    version=get_version(),  # Don't import ml_workflow!
     description='A scikit-learn-based model that incorporates ML pipelines, hyperparameter optimization, and calibration all within a cross-validation framework', 
     long_description=long_description,
     long_description_content_type='text/markdown',  
     url='https://github.com/WarnOnForecast/ml_workflow', 
     author='NOAA National Severe Storms Laboratory', 
-    classifiers=[  # Optional
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
+    classifiers=[
         'Development Status :: 3 - Alpha',
-        'Intended Audience :: Scientists',
-        'Programming Language :: Python :: 3'
+        'Intended Audience :: Science/Research',  # Fixed typo
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
-    install_requires = [
-        'scikit-learn==1.0.2',
-        'scikit-image>=0.18.1',
-        'xarray',
-        'imblearn',
+    install_requires=[
+        'scikit-learn>=1.0.2',  # Changed == to >= for flexibility
+        'scikit-learn-intelex>=2023.0.1',
+        'scikit-image>=0.19',
+        'xarray>=0.18',
+        'imbalanced-learn',
+        'pandas>-1.3.0',
         'hyperopt',
-        'lightgbm'
+        'lightgbm',
+        'numpy>=1.20',  # Add explicit numpy requirement
     ],
-    packages=['ml_workflow', 'ml_workflow.io', 'ml_workflow.preprocess'],  # Required
+    packages=find_packages(),  # Use find_packages() instead of manual list
     python_requires='>=3.8, <4',
-    package_dir={'ml_workflow': 'ml_workflow'},
-    project_urls={  # Optional
-        'Bug Reports': 'https://github.com/WarnOnForecast/ml_workflow/issues',
-        'Source': 'https://github.com/WarnOnForecast/ml_workflow',
+    project_urls={
+        'Bug Reports': 'https://github.com/monte-flora/ml_workflow/issues',
+        'Source': 'https://github.com/monte-flora/ml_workflow',
     },
 )
